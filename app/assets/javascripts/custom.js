2e127fb57db9896f06f3501cb544dde0ba16e7cb
carloperaudo <carlo.peraudo@gmail.com>
2017-11-28 16:11:51 +0100

    Pages labels upd

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -5,3 +5,12 @@
 //
 //
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110272632-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-110272632-1');
+  gtag('config', 'GA_TRACKING_ID', { 'anonymize_ip': true });
+</script>
