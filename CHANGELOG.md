ceaa09e5fbbbe8d8d010a31d4f6cf9ee5e3b7305
carloperaudo <carlo.peraudo@gmail.com>
2018-01-23 09:44:39 +0100

    CHANGELOG.md upd v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.1.0](https://github.com/csipiemonte/consul/compare/v1.0.1...v1.1.0) - 2018-01-23
+### Added
+- SPID authentication
+- Symlink to data/public/system (docs and images)
+- Symlink to data/tmp
+
+### Changed
+- Log file path
+
 ## [1.0.1](https://github.com/csipiemonte/consul/compare/v1.0.0...v1.0.1) - 2017-12-05
 ### Changed
 - Welcome page CSS
--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,10 +3,11 @@ if Rails.env.development? || Rails.env.staging? || Rails.env.preproduction?
   Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 elsif Rails.env.test?
   Delayed::Worker.delay_jobs = false
-  Delayed::Worker.logger = Logger.new("/var/log/rails/tst-www-deciditorino.portali.csi.it_443/delayed_job.log", 5, 30*1048576)
+  Delayed::Worker.logger = Logger.new("/var/log/rails/tst-www-deciditorino.portali.csi.it_443/delayed_job.log")
 else
+  # production
   Delayed::Worker.delay_jobs = true
-  Delayed::Worker.logger = Logger.new("/var/log/rails/www.deciditorino.it_443/delayed_job.log", 5, 30*1048576)
+  Delayed::Worker.logger = Logger.new("/var/log/rails/www.deciditorino.it_443/delayed_job.log")
 end
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 2
