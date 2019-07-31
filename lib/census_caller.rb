562930d0a6fbf9b550c7772a4f910abf9271f7e0
carloperaudo <carlo.peraudo@gmail.com>
2019-07-31 14:30:14 +0200

    New settings feature.remote_census

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -1,7 +1,11 @@
 class CensusCaller
 
-  def call(document_type, document_number)
-    response = CensusCsiApi.new.call(document_number)
+  def call(document_type, document_number, date_of_birth, postal_code)
+    if Setting["feature.remote_census"].present?
+      response = RemoteCensusApi.new.call(document_type, document_number, date_of_birth, postal_code)
+    else
+      response = CensusCsiApi.new.call(document_number)
+    end
     response = LocalCensus.new.call(document_type, document_number) unless response.valid?
 
     response
